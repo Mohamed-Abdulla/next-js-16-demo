9debb7df9195ff3370ad713884409c43e72fabf9
Mohamed Abdulla <abdabdulla78@gmail.com>
2025-11-09 23:44:26 +0530

    refactor(Navbar): improve link handling and structure for navigation items

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,6 +11,7 @@ import { NAV_LINKS } from "@/constants/navlinks";
 import { useTheme } from "next-themes";
 import Image from "next/image";
 import { ModeToggle } from "./toggle-theme";
+import Link from "next/link";
 
 export const Navbar: FC = () => {
   const { theme, systemTheme } = useTheme();
@@ -18,6 +19,7 @@ export const Navbar: FC = () => {
 
   useEffect(() => {
     setMounted(true);
+    console.log("iam called");
   }, []);
 
   const currentTheme = theme === "system" ? systemTheme : theme;
@@ -28,24 +30,30 @@ export const Navbar: FC = () => {
         <NavigationMenuList>
           {NAV_LINKS.map((link) => (
             <NavigationMenuItem key={link.id}>
-              <NavigationMenuLink href={link.href} className="font-medium">
-                {link.href === "/" ? (
-                  <div className="flex items-center gap-x-2">
-                    {mounted && (
-                      <Image
-                        src={`/assets/${
-                          currentTheme === "dark" ? "logo-white" : "logo"
-                        }.svg`}
-                        alt="Logo"
-                        width={120}
-                        height={30}
-                      />
-                    )}
-                    <div className="h-6 border-r border-r-orange-600" />
-                  </div>
-                ) : (
-                  link.title
-                )}
+              <NavigationMenuLink
+                href={link.href}
+                className="font-medium"
+                asChild
+              >
+                <Link href={link.href}>
+                  {link.href === "/" ? (
+                    <div className="flex items-center gap-x-2">
+                      {mounted && (
+                        <Image
+                          src={`/assets/${
+                            currentTheme === "dark" ? "logo-white" : "logo"
+                          }.svg`}
+                          alt="Logo"
+                          width={120}
+                          height={30}
+                        />
+                      )}
+                      <div className="h-6 border-r border-r-orange-600" />
+                    </div>
+                  ) : (
+                    link.title
+                  )}
+                </Link>
               </NavigationMenuLink>
             </NavigationMenuItem>
           ))}
