2ffae1bc7b746c5bd11491d1256d7e1db8808042
Mohamed Abdulla <abdabdulla78@gmail.com>
2025-11-09 23:30:17 +0530

    feat: refactor Navbar component to improve theme handling and state management

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { FC } from "react";
 
+import { FC, useEffect, useState } from "react";
 import {
   NavigationMenu,
   NavigationMenuItem,
@@ -12,29 +12,35 @@ import { useTheme } from "next-themes";
 import Image from "next/image";
 import { ModeToggle } from "./toggle-theme";
 
-interface NavbarProps {}
+export const Navbar: FC = () => {
+  const { theme, systemTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
 
-export const Navbar: FC<NavbarProps> = ({}) => {
-  const { theme } = useTheme();
-  const isDark =
-    theme === "dark" ||
-    (theme === "system" &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const currentTheme = theme === "system" ? systemTheme : theme;
+
   return (
     <nav className="flex w-full items-center justify-between border-b p-3 backdrop-blur-sm">
       <NavigationMenu>
-        <NavigationMenuList className="">
+        <NavigationMenuList>
           {NAV_LINKS.map((link) => (
             <NavigationMenuItem key={link.id}>
               <NavigationMenuLink href={link.href} className="font-medium">
                 {link.href === "/" ? (
                   <div className="flex items-center gap-x-2">
-                    <Image
-                      src={`/assets/${isDark ? "logo-white" : "logo"}.svg`}
-                      alt="Logo"
-                      width={120}
-                      height={30}
-                    />
+                    {mounted && (
+                      <Image
+                        src={`/assets/${
+                          currentTheme === "dark" ? "logo-white" : "logo"
+                        }.svg`}
+                        alt="Logo"
+                        width={120}
+                        height={30}
+                      />
+                    )}
                     <div className="h-6 border-r border-r-orange-600" />
                   </div>
                 ) : (
